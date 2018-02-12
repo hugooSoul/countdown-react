636854d2aba7b207781c695d1f889459a473f69b
Hugo López Mandujano <hugol.soul@gmail.com>
2018-02-12 12:50:33 -0600

    using react-awesome-countdowntimer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import CountdownTimer from 'react-awesome-countdowntimer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h2>Countdown Timer</h2>
+        <div className="container">
+          <h2>Countdown Timer</h2>
+          <CountdownTimer endDate={moment('02/28/2018')}/>
+        </div>
       </div>
     );
   }
